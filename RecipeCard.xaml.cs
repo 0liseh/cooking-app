e9b43b845bd302d94dfb08273f1c4a5647e16f54
jessica.hoang1@ucalgary.ca <60759765+jessicahoang1@users.noreply.github.com>
2022-12-08 16:31:52 -0700

    Hard coded in the images, title, rating, duration, difficulty to "dynamically" change depending on search results.

--- RecipeCard.xaml.cs
+++ RecipeCard.xaml.cs
@@ -20,6 +20,67 @@ namespace cooking_app
     /// </summary>
     public partial class RecipeCard : UserControl
     {
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                this.TitleSearchText.Content = this.title;
+            }
+
+        }
+
+        private string duration;
+        public string Duration
+        {
+            get { return duration; }
+            set
+            {
+                duration = value;
+                this.DurationSearchText.Content = this.duration;
+            }
+
+        }
+
+        private string rating;
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                rating = value;
+                this.RatingSearchText.Content = this.rating;
+            }
+
+        }
+
+        private string difficulty;
+        public string Difficulty
+        {
+            get { return difficulty; }
+            set
+            {
+                difficulty = value;
+                this.DifficultySearchText.Content = this.difficulty;
+            }
+
+        }
+
+        private ImageSource img;
+        public ImageSource ImageCard
+        {
+            get { return img; }
+            set
+            {
+                img = value;
+                this.ImgSearchCard.Source = this.img;
+            }
+
+        }
+
         public RecipeCard()
         {
             InitializeComponent();
--- SearchResultsPage.xaml.cs
+++ SearchResultsPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace cooking_app
 {
@@ -20,16 +21,59 @@ namespace cooking_app
     /// </summary>
     public partial class SearchResultsPage : Page
     {
+        String searchResult = "Ramen";
+
+        string[] ramenResults = { "Miso Ramen", "Shoyu Ramen", "Tonkotsu Ramen", "Shio Ramen", "Hakodate", "Hiroshima", "Sapporo Ramen", "Takayama", "Tsukemen", "Tokyo Ramen" };
+        string[] ramenImg = { "MisoRamen.jpg", "ShoyuRamen.jpg", "Tonkatsu.jpg", "ShioRamen.jpg", "Hakodate.jpg", "Hiroshima.jpg", "Sapporo.jpg", "Takayama.jpg", "Tsukemen.jpg", "TokyoRamen.jpg" };
+        string[] ramenRating = { "5.0", "5.0", "4.9", "4.8", "4.6", "4.5", "4.5", "4.5", "4.5", "4.3" };
+
+        string[] macResults = { "Creamy Mac and Cheese", "Mac and Cheese", "Mac and Extra Cheese", "Baked Mac and Cheese", "Southern Mac and Cheese", "White Chedder Mac and Cheese", "Ultra Creamy Mac and Cheese", "3 Cheese Mac and Cheese", "Brie Mac and Cheese", "Simple Mac and Cheese" };
+        string[] macRating = { "4.8", "4.8", "4.8", "4.7", "4.5", "4.4", "4.3", "4.3", "4.2", "4.0" };
+        
+        string[] macImg = { "MisoRamen.jpg", "ShoyuRamen.jpg", "Tonkatsu.jpg", "ShioRamen.jpg", "Hakodate.jpg", "Hiroshima.jpg", "Sapporo.jpg", "Takayama.jpg", "Tsukemen.jpg", "TokyoRamen.jpg" };
+
         public SearchResultsPage()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 10; i++)
+            if (searchResult.Contains("ramen", StringComparison.InvariantCultureIgnoreCase))
             {
-                RecipeCard rcard = new RecipeCard();
-                //rcard.Title = "Chocolate" + i.ToString();
-                this.SearchResults.Children.Add(rcard);
+                searchBar.Text = searchResult;
+                for (int i = 0; i < 10; i++)
+                {
+                    RecipeCard rcard = new RecipeCard();
+                    rcard.Title = ramenResults[i];
+                    rcard.Difficulty = "EASY";
+                    rcard.Rating = ramenRating[i];
+                    rcard.Duration = "30 MIN";
+                    Uri uri = new Uri("pack://application:,,,/cooking-app;component/img/ramen/" + ramenImg[i], UriKind.Absolute);
+                    ImageSource imgSource = new BitmapImage(uri);
+                    rcard.ImageCard = imgSource;
+                    this.SearchResults.Children.Add(rcard);
+                }
+            } else if (searchResult.Contains("mac and cheese"))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    RecipeCard rcard = new RecipeCard();
+                    rcard.Title = macResults[i];
+                    rcard.Difficulty = "EASY";
+                    rcard.Rating = macRating[i];
+                    rcard.Duration = "30 MIN";
+                    // change to folder mac and cheese
+                    Uri uri = new Uri("pack://application:,,,/cooking-app;component/img/ramen/" + macImg[i], UriKind.Absolute);
+                    ImageSource imgSource = new BitmapImage(uri);
+                    rcard.ImageCard = imgSource;
+                    this.SearchResults.Children.Add(rcard);
+                }
             }
+
+        }
+
+        private void searchBar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            searchBar.Text = "";
+
         }
     }
 }
