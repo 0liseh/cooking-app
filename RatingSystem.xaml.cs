aee1372c7196dd22b9645d1cea7ceecfa67dc3c6
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2022-12-07 18:25:10 -0700

    rating system done

--- RatingSystem.xaml.cs
+++ RatingSystem.xaml.cs
@@ -28,66 +28,92 @@ namespace cooking_app
 
         public void ChangeRating(object sender, RoutedEventArgs e)
         {
-            
+            IEnumerable<ToggleButton> stars = RatingSystemStars.Children.OfType<ToggleButton>();
+
             if (sender.Equals(FirstStar))
             {
-                foreach (ToggleButton star in RatingSystemStars.Children.OfType<ToggleButton>())
+                
+                if (stars.ElementAt(0).IsChecked ?? false)
                 {
-
-                    if (star.Name == "FirstButton")
-                    {
-                        star.IsChecked = true;
-                        return;
-                    }             
-
+                    stars.ElementAt(0).IsChecked = true;
+                    
                 }
+                else
+                {
+                    stars.ElementAt(0).IsChecked = false;
+                    stars.ElementAt(1).IsChecked = false;
+                    stars.ElementAt(2).IsChecked = false;
+                    stars.ElementAt(3).IsChecked = false;
+                    stars.ElementAt(4).IsChecked = false;
+                }
+
             }
             else if (sender.Equals(SecondStar))
             {
-                foreach (ToggleButton star in RatingSystemStars.Children.OfType<ToggleButton>())
+                stars.ElementAt(0).IsChecked = true;
+
+                if (stars.ElementAt(1).IsChecked ?? false)
                 {
-                    if (star.Name == "FirstButton")
-                    {
-                        star.IsChecked = true;
-                        
-                    }
-
-                    if (star.Name == "SecondButton")
-                    {
-                        star.IsChecked = true;
-
-                    }
-
-                    if (star.Name == "ThirdButton")
-                    {
-                        star.IsChecked = false;
-
-                    }
-
-                    if (star.Name == "FourthButton")
-                    {
-                        star.IsChecked = false;
-
-                    }
-
-                    if (star.Name == "FifthButton")
-                    {
-                        star.IsChecked = false;
-
-                    }
-
+                    stars.ElementAt(1).IsChecked = true;
+                    
                 }
+                else
+                {
+                    stars.ElementAt(1).IsChecked = false;
+                    stars.ElementAt(2).IsChecked = false;
+                    stars.ElementAt(3).IsChecked = false;
+                    stars.ElementAt(4).IsChecked = false;
+                }
+
             }
             else if (sender.Equals(ThirdStar))
             {
+                stars.ElementAt(0).IsChecked = true;
+                stars.ElementAt(1).IsChecked = true;
 
+                if (stars.ElementAt(2).IsChecked ?? false)
+                {
+                    stars.ElementAt(2).IsChecked = true;
+                    
+                }
+                else
+                {
+                    stars.ElementAt(2).IsChecked = false;
+                    stars.ElementAt(3).IsChecked = false;
+                    stars.ElementAt(4).IsChecked = false;
+                }
             }
             else if (sender.Equals(FourthStar))
             {
+                stars.ElementAt(0).IsChecked = true;
+                stars.ElementAt(1).IsChecked = true;
+                stars.ElementAt(2).IsChecked = true;
 
+                if (stars.ElementAt(3).IsChecked ?? false)
+                {
+                    stars.ElementAt(3).IsChecked = true;
+                    
+                }
+                else
+                {
+                    stars.ElementAt(3).IsChecked = false;
+                    stars.ElementAt(4).IsChecked = false;
+                }
             }
             else if (sender.Equals(FifthStar))
             {
+                stars.ElementAt(0).IsChecked = true;
+                stars.ElementAt(1).IsChecked = true;
+                stars.ElementAt(2).IsChecked = true;
+                stars.ElementAt(3).IsChecked = true;
+                if (stars.ElementAt(4).IsChecked ?? false)
+                {
+                    stars.ElementAt(4).IsChecked = true;
+                }
+                else
+                {
+                    stars.ElementAt(4).IsChecked = false;
+                }
 
             }
 
